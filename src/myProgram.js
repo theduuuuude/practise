4adeddaffa693e8d074d4c5d2b28b2a3d4bcaffb
the.duuuuude <the.duuuuude@gmail.com>
2018-03-12 00:40:22 +0100

    small improvements

--- src/myProgram.js
+++ src/myProgram.js
@@ -8,11 +8,12 @@ const heroCards = ['As','Ac'];
 const heroHand = heroCards.join()
 const deck = _.difference(cards,heroCards)
 
-const hands = _.flatten(deck.map(card1=>deck.map(card2=>{
+const hands = _.uniq(_.flatten(deck.map(card1=>deck.map(card2=>{
     if(card1!=card2){
-        return `${card1}${card2}`
+        
+        return [card1,card2].sort((a,b)=>a-b).join()
     }
-}))).filter(val=>val)
+}))).filter(val=>val))
 
 let database = []
 
@@ -27,12 +28,11 @@ hands.map((hand,ind)=>{
     let essential = equityVillain + tie/2
 //     console.log(`Hand ${hand} ${chalk.greenBright('processed')}. Index is ${ind}
 // this length is ${database.length}`)
-    let s = database.sort((a,b)=>parseFloat(a.equity)<parseFloat(b.equity))
-    database.length > 2 ? console.log(`Best hands so far: ${s[0].hand} with ${s[0].equity}% equity,${s[1].hand} with ${s[1].equity}% equity`) : null
-    console.log(database)
+    
     database.push({
         equity:essential,
         hand:hand
     })
-
+    let s = database.sort((a,b)=>b.equity-a.equity)
+    database.length > 2 ? console.log(`Best hands so far: ${s[0].hand} with ${s[0].equity}% equity,${s[1].hand} with ${s[1].equity}% equity`) : null
 })
